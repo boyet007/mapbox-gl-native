15f8cd247cc7c0526d1adf321349859a7bdfa47c
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-11-06 20:39:13 +0200

    [core] Refactor HeadlessBackend

--- platform/default/headless_backend_osmesa.cpp
+++ platform/default/headless_backend_osmesa.cpp
@@ -1,47 +1,58 @@
 #include <mbgl/platform/default/headless_backend.hpp>
 #include <mbgl/platform/log.hpp>
 
+#include <GL/osmesa.h>
+
+#include <cassert>
+
 namespace mbgl {
+
+struct OSMesaImpl : public HeadlessBackend::Impl {
+    OSMesaImpl(OSMesaContext glContext_) : glContext(glContext_) {
+    }
+
+    ~OSMesaImpl() {
+        if (glContext != OSMesaGetCurrentContext()) {
+            activateContext();
+        }
+        OSMesaDestroyContext(glContext);
+    }
+
+    void activateContext() final {
+        if (!OSMesaMakeCurrent(glContext, &fakeBuffer, GL_UNSIGNED_BYTE, 1, 1)) {
+            throw std::runtime_error("Switching OpenGL impl failed.\n");
+        }
+    }
+
+    OSMesaContext glContext = nullptr;
+    GLubyte fakeBuffer = 0;
+};
 
 gl::glProc HeadlessBackend::initializeExtension(const char* name) {
     return OSMesaGetProcAddress(name);
 }
 
+bool HeadlessBackend::hasContext() const {
+    return bool(impl);
+};
+
+bool HeadlessBackend::hasDisplay() {
+    return true;
+};
+
 void HeadlessBackend::createContext() {
-    if (glContext == nullptr) {
+    assert(!hasContext());
+
 #if OSMESA_MAJOR_VERSION * 100 + OSMESA_MINOR_VERSION >= 305
-        glContext = OSMesaCreateContextExt(OSMESA_RGBA, 16, 0, 0, nullptr);
+    OSMesaContext glContext = OSMesaCreateContextExt(OSMESA_RGBA, 16, 0, 0, nullptr);
 #else
-        glContext = OSMesaCreateContext(OSMESA_RGBA, nullptr);
+    OSMesaContext glContext = OSMesaCreateContext(OSMESA_RGBA, nullptr);
 #endif
-        if (glContext == nullptr) {
-            Log::Error(Event::OpenGL, "failed to create OSMesa context");
-        }
-    }
-
     if (glContext == nullptr) {
-        throw std::runtime_error("Error creating GL context object.");
+        throw std::runtime_error("Error creating GL impl object.");
     }
-}
 
-void HeadlessBackend::destroyContext() {
-    if (glContext) {
-        if (glContext != OSMesaGetCurrentContext()) {
-            activateContext();
-        }
-        OSMesaDestroyContext(glContext);
-        glContext = nullptr;
-    }
-}
-
-void HeadlessBackend::activateContext() {
-    if (!OSMesaMakeCurrent(glContext, &fakeBuffer, GL_UNSIGNED_BYTE, 1, 1)) {
-        throw std::runtime_error("Switching OpenGL context failed.\n");
-    }
-}
-
-void HeadlessBackend::deactivateContext() {
-    // no-op.
+    impl.reset(new OSMesaImpl(glContext));
 }
 
 } // namespace mbgl
--- platform/linux/src/headless_backend_glx.cpp
+++ platform/linux/src/headless_backend_glx.cpp
@@ -3,65 +3,89 @@
 
 #include <mbgl/platform/log.hpp>
 
-// #include <cassert>
+#include <cassert>
 
 #include <GL/glx.h>
 
 namespace mbgl {
 
+struct GLXImpl : public HeadlessBackend::Impl {
+    GLXImpl(GLXContext glContext_, GLXPbuffer glxPbuffer_, Display* xDisplay_, GLXFBConfig* fbConfigs_)
+            : glContext(glContext_),
+              glxPbuffer(glxPbuffer_),
+              xDisplay(xDisplay_),
+              fbConfigs(fbConfigs_) {
+    }
+
+    ~GLXImpl() {
+        if (glxPbuffer) {
+            glXDestroyPbuffer(xDisplay, glxPbuffer);
+        }
+        glXDestroyContext(xDisplay, glContext);
+    }
+
+    void activateContext() final {
+        if (!glXMakeContextCurrent(xDisplay, glxPbuffer, glxPbuffer, glContext)) {
+            throw std::runtime_error("Switching OpenGL impl failed.\n");
+        }
+    }
+
+    void deactivateContext() final {
+        if (!glXMakeContextCurrent(xDisplay, 0, 0, nullptr)) {
+            throw std::runtime_error("Removing OpenGL impl failed.\n");
+        }
+    }
+
+    GLXContext glContext = nullptr;
+    GLXPbuffer glxPbuffer = 0;
+
+    // Needed for ImplDeleter.
+    Display* xDisplay = nullptr;
+    GLXFBConfig* fbConfigs = nullptr;
+};
+
 gl::glProc HeadlessBackend::initializeExtension(const char* name) {
     return glXGetProcAddress(reinterpret_cast<const GLubyte*>(name));
 }
 
-void HeadlessBackend::createContext() {
-    xDisplay = display->attribute<Display*>();
-    fbConfigs = display->attribute<GLXFBConfig*>();
+bool HeadlessBackend::hasContext() const {
+    return bool(impl);
+};
 
-    if (!glContext) {
-        // Try to create a legacy context
-        glContext = glXCreateNewContext(xDisplay, fbConfigs[0], GLX_RGBA_TYPE, None, True);
-        if (glContext) {
-            if (!glXIsDirect(xDisplay, glContext)) {
-                Log::Error(Event::OpenGL, "failed to create direct OpenGL Legacy context");
-                glXDestroyContext(xDisplay, glContext);
-                glContext = nullptr;
-            }
-        }
+bool HeadlessBackend::hasDisplay() {
+    if (!display) {
+        display.reset(new HeadlessDisplay);
     }
+    return bool(display);
+};
 
+void HeadlessBackend::createContext() {
+    assert(!hasContext());
+
+    Display* xDisplay = display->attribute<Display*>();
+    GLXFBConfig* fbConfigs = display->attribute<GLXFBConfig*>();
+
+    // Try to create a legacy impl.
+    GLXContext glContext = glXCreateNewContext(xDisplay, fbConfigs[0], GLX_RGBA_TYPE, None, True);
+    if (glContext && !glXIsDirect(xDisplay, glContext)) {
+        Log::Error(Event::OpenGL, "failed to create direct OpenGL Legacy impl");
+        glXDestroyContext(xDisplay, glContext);
+        glContext = nullptr;
+    }
     if (glContext == nullptr) {
-        throw std::runtime_error("Error creating GL context object.");
+        throw std::runtime_error("Error creating GL impl object.");
     }
 
     // Create a dummy pbuffer. We will render to framebuffers anyway, but we need a pbuffer to
-    // activate the context.
+    // activate the impl.
     int pbufferAttributes[] = {
         GLX_PBUFFER_WIDTH, 8,
         GLX_PBUFFER_HEIGHT, 8,
         None
     };
-    glxPbuffer = glXCreatePbuffer(xDisplay, fbConfigs[0], pbufferAttributes);
-}
+    GLXPbuffer glxPbuffer = glXCreatePbuffer(xDisplay, fbConfigs[0], pbufferAttributes);
 
-void HeadlessBackend::destroyContext() {
-    if (glxPbuffer) {
-        glXDestroyPbuffer(xDisplay, glxPbuffer);
-        glxPbuffer = 0;
-    }
-
-    glXDestroyContext(xDisplay, glContext);
-}
-
-void HeadlessBackend::activateContext() {
-    if (!glXMakeContextCurrent(xDisplay, glxPbuffer, glxPbuffer, glContext)) {
-        throw std::runtime_error("Switching OpenGL context failed.\n");
-    }
-}
-
-void HeadlessBackend::deactivateContext() {
-    if (!glXMakeContextCurrent(xDisplay, 0, 0, nullptr)) {
-        throw std::runtime_error("Removing OpenGL context failed.\n");
-    }
+    impl.reset(new GLXImpl(glContext, glxPbuffer, xDisplay, fbConfigs));
 }
 
 } // namespace mbgl
